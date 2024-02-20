37cc021332039a877d7db2d87220c389052a33ae
trgpnt <trungpnt0605@gmail.com>
2024-02-21 02:22:50 +0700

    Running app, enable copy-paste

--- lib/word_details_screen.dart
+++ lib/word_details_screen.dart
@@ -1,11 +1,7 @@
-// word_details_screen.dart
-
 import 'package:flutter/material.dart';
 
 class WordDetailsScreen extends StatefulWidget {
   final Map<String, String> wordDetails;
-
-  //TODO eval if needed?
   final String searchQuery;
 
   const WordDetailsScreen({
@@ -20,6 +16,8 @@ class WordDetailsScreen extends StatefulWidget {
 
 class _WordDetailsScreenState extends State<WordDetailsScreen>
     with AutomaticKeepAliveClientMixin {
+  FocusNode _focusNode = FocusNode();
+
   @override
   bool get wantKeepAlive => true;
 
@@ -30,41 +28,55 @@ class _WordDetailsScreenState extends State<WordDetailsScreen>
     final String word = widget.wordDetails['word'] ?? '';
     final String meaning = widget.wordDetails['meaning'] ?? '';
 
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Word Details'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Container for the search query with light green background
-            Container(
-              color: Colors.lightGreen,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  '${word}',
-                  style: TextStyle(color: Colors.white),
+    return GestureDetector(
+      onTap: () {
+        // Unfocus the SelectableText when tapping on the void space
+        _focusNode.unfocus();
+      },
+      child: Scaffold(
+        appBar: AppBar(
+          title: Text('Word Details'),
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              // Container for the search query with light green background
+              Focus(
+                onFocusChange: (hasFocus) {
+                  if (!hasFocus) {
+                    _focusNode.unfocus();
+                  }
+                },
+                child: Container(
+                  color: Colors.lightGreen,
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: SelectableText(
+                      '${word}',
+                      style: TextStyle(color: Colors.white),
+                      focusNode: _focusNode,
+                    ),
+                  ),
                 ),
               ),
-            ),
-            const SizedBox(height: 16),
-            // Container for the meaning with yellow background
-            Container(
-              color: Colors.yellow,
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Meaning: $meaning',
-                  style: const TextStyle(
-                      color: Colors.black), // You can adjust the text color
+              const SizedBox(height: 16),
+              // Container for the meaning with yellow background
+              Container(
+                color: Colors.yellow,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: SelectableText(
+                    'Meaning: $meaning',
+                    style: const TextStyle(
+                        color: Colors.black), // You can adjust the text color
+                  ),
                 ),
               ),
-            ),
-            // Add more details as needed
-          ],
+              // Add more details as needed
+            ],
+          ),
         ),
       ),
     );
