5da0fd043861c7940be4c0a327df74f6fd4e1cb8
trgpnt <trungpnt0605@gmail.com>
2024-02-21 02:12:43 +0700

    Running app, Added loading screen

---
+++ lib/loading_screen.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class LoadingScreen extends StatelessWidget {
+  const LoadingScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets/loading.gif', width: 150, height: 150),
+            // Replace with your cat image
+            const SizedBox(height: 16),
+            const CircularProgressIndicator(),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,48 @@
 import 'package:flutter/material.dart';
+
 import 'dictionary_app.dart';
+import 'loading_screen.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pali Dictionary',
+      home: FutureBuilder<void>(
+        // Simulate some asynchronous initialization
+        future: init(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            return MyHomePage(); // Transition to the main page
+          } else {
+            return LoadingScreen(); // Show the loading screen while initializing
+          }
+        },
+      ),
+    );
+  }
+
+  Future<void> init() async {
+    // Perform your initialization tasks here
+    // This could be loading data, setting up services, etc.
+    await Future.delayed(const Duration(seconds: 2)); // Simulating a delay
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  // Your existing code for the dictionary app here...
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
