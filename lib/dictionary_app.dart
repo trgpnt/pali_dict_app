2faaf846279b8b645696cad643a715816430749c
trgpnt <trungpnt0605@gmail.com>
2024-02-23 00:18:11 +0700

    Refactored flutter pattern. _ Must prioritise VNese meaning.

--- lib/dictionary_app.dart
+++ lib/dictionary_app.dart
@@ -1,7 +1,5 @@
-import 'dart:convert';
-
+import 'package:dict_app/service_layer/dictionary_read_service.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart' show ByteData, rootBundle;
 
 import 'search_results.dart';
 
@@ -18,6 +16,7 @@ class _DictionaryAppState extends State<DictionaryApp> {
   TranslationMode currentMode = TranslationMode.paliToEnglish;
   Map<String, String> selectedWord = {};
   String currentSearchQuery = '';
+  final DictionaryReadService _dictionaryReadService = DictionaryReadService();
 
   @override
   void initState() {
@@ -28,52 +27,14 @@ class _DictionaryAppState extends State<DictionaryApp> {
 
   Future<void> loadDictionaryData() async {
     try {
-      await loadDictionary(
-        'assets/english-pali_Ven_A_P_Buddhadatta-2.4.2.txt',
-        TranslationMode.englishToPali,
-      );
-      await loadDictionary(
-        'assets/pali-english_Ven_A_P_Buddhadatta-2.4.2.txt',
-        TranslationMode.paliToEnglish,
-      );
-      await loadDictionary(
-        'assets/conbimed_pali_vnese.txt',
-        TranslationMode.paliToVNese,
-      );
-    } catch (e) {
-      print('Error loading dictionary data: $e');
-    }
-  }
-
-  Future<void> loadDictionary(String assetPath, TranslationMode mode) async {
-    try {
-      final ByteData data = await rootBundle.load(assetPath);
-      final String content = utf8.decode(data.buffer.asUint8List());
-
-      List<String> lines = LineSplitter.split(content).toList();
-      List<Map<String, String>> entries = [];
-
-      String currentEntry = '';
-      for (String line in lines) {
-        if (line.contains('\t')) {
-          final List<String> parts = line.split('\t');
-          if (parts.length == 2) {
-            entries.add({'word': parts[0], 'meaning': parts[1]});
-          }
-        } else if (line.trim() == '.') {
-          if (currentEntry.isNotEmpty) {
-            final List<String> parts = currentEntry.split('\t');
-            if (parts.length == 2) {
-              entries.add({'word': parts[0], 'meaning': parts[1]});
-            }
-          }
-          currentEntry = '';
-        } else {
-          currentEntry += line;
-        }
-      }
-
-      dictionaryData[mode] = entries;
+      dictionaryData[TranslationMode.englishToPali] =
+          await _dictionaryReadService.loadDictionary(
+              'assets/english-pali_Ven_A_P_Buddhadatta-2.4.2.txt');
+      dictionaryData[TranslationMode.paliToEnglish] =
+          await _dictionaryReadService.loadDictionary(
+              'assets/pali-english_Ven_A_P_Buddhadatta-2.4.2.txt');
+      dictionaryData[TranslationMode.paliToVNese] = await _dictionaryReadService
+          .loadDictionary('assets/conbimed_pali_vnese.txt');
     } catch (e) {
       print('Error loading dictionary data: $e');
     }
@@ -127,9 +88,6 @@ class _DictionaryAppState extends State<DictionaryApp> {
     return Container(
       color: Colors.yellow,
       child: Scaffold(
-        appBar: AppBar(
-          title: const Text('Pali Dictionary'),
-        ),
         body: Padding(
           padding: const EdgeInsets.all(16.0),
           child: Column(
@@ -149,12 +107,16 @@ class _DictionaryAppState extends State<DictionaryApp> {
                         selectedWord = {};
                       });
                     },
-                    items: TranslationMode.values.map((mode) {
+                    items: TranslationMode.values
+                        .where((mode) =>
+                            mode !=
+                            TranslationMode.paliToVNese) // Skip paliToVNese
+                        .map((mode) {
                       return DropdownMenuItem<TranslationMode>(
                         value: mode,
                         child: Text(
                           mode == TranslationMode.paliToEnglish
-                              ? 'Pali to English'
+                              ? 'Pali to English / Vietnamese'
                               : 'English to Pali',
                           style: TextStyle(
                             color: Colors.black87,
@@ -340,10 +302,4 @@ class WordDetailsWidget extends StatelessWidget {
       ],
     );
   }
-}
-
-void main() {
-  runApp(MaterialApp(
-    home: DictionaryApp(),
-  ));
 }
---
+++ lib/service_layer/dictionary_read_service.dart
@@ -0,0 +1,51 @@
+// dictionary_read_service.dart
+import 'dart:convert';
+
+import 'package:flutter/services.dart' show ByteData, rootBundle;
+
+class DictionaryReadService {
+  // Private constructor
+  DictionaryReadService._();
+
+  // Singleton instance
+  static final DictionaryReadService _instance = DictionaryReadService._();
+
+  // Getter for the instance
+  factory DictionaryReadService() {
+    return _instance;
+  }
+
+  Future<List<Map<String, String>>> loadDictionary(String assetPath) async {
+    try {
+      final ByteData data = await rootBundle.load(assetPath);
+      final String content = utf8.decode(data.buffer.asUint8List());
+
+      List<String> lines = LineSplitter.split(content).toList();
+      List<Map<String, String>> entries = [];
+
+      String currentEntry = '';
+      for (String line in lines) {
+        if (line.contains('\t')) {
+          final List<String> parts = line.split('\t');
+          if (parts.length == 2) {
+            entries.add({'word': parts[0], 'meaning': parts[1]});
+          }
+        } else if (line.trim() == '.') {
+          if (currentEntry.isNotEmpty) {
+            final List<String> parts = currentEntry.split('\t');
+            if (parts.length == 2) {
+              entries.add({'word': parts[0], 'meaning': parts[1]});
+            }
+          }
+          currentEntry = '';
+        } else {
+          currentEntry += line;
+        }
+      }
+      return entries;
+    } catch (e) {
+      print('Error loading dictionary data: $e');
+      return [];
+    }
+  }
+}
