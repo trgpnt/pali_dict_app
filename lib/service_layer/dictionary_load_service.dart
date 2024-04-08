2d11cba871f228b90947253a58c2b918fec8cf70
trgpnt <trungpnt0605@gmail.com>
2024-04-08 22:09:14 +0700

    Running app, _ Optimized for mobile execution

--- lib/service_layer/dictionary_load_service.dart
+++ lib/service_layer/dictionary_load_service.dart
@@ -19,28 +19,30 @@ class DictionaryLoadService {
     try {
       final ByteData data = await rootBundle.load(assetPath);
       final String content = utf8.decode(data.buffer.asUint8List());
-      final List<String> lines = LineSplitter.split(content).toList();
 
-      // Splitting lines into two halves
-      final int halfLength = (lines.length / 2).ceil();
-      final List<String> firstHalfLines = lines.sublist(0, halfLength);
-      final List<String> secondHalfLines = lines.sublist(halfLength);
+      List<String> lines = LineSplitter.split(content).toList();
+      List<Map<String, String>> entries = [];
 
-      // Processing first half of lines
-      final Future<List<Map<String, String>>> firstHalfFuture =
-          _processLines(firstHalfLines);
-
-      // Processing second half of lines
-      final Future<List<Map<String, String>>> secondHalfFuture =
-          _processLines(secondHalfLines);
-
-      // Waiting for both halves to finish and combining the results
-      final List<List<Map<String, String>>> results =
-          await Future.wait([firstHalfFuture, secondHalfFuture]);
-      final List<Map<String, String>> combinedResults =
-          results.expand((list) => list).toList();
-
-      return combinedResults;
+      String currentEntry = '';
+      for (String line in lines) {
+        if (line.contains('\t')) {
+          final List<String> parts = line.split('\t');
+          if (parts.length == 2) {
+            entries.add({'word': parts[0], 'meaning': parts[1]});
+          }
+        } else if (line.trim() == '.') {
+          if (currentEntry.isNotEmpty) {
+            final List<String> parts = currentEntry.split('\t');
+            if (parts.length == 2) {
+              entries.add({'word': parts[0], 'meaning': parts[1]});
+            }
+          }
+          currentEntry = '';
+        } else {
+          currentEntry += line;
+        }
+      }
+      return entries;
     } catch (e) {
       print('Error loading dictionary data: $e');
       return [];
